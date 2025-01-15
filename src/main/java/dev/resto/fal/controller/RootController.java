120fd1a841755fdaf89a037efe3f720c99e665ef
fewdw <frederic.alefebvre@gmail.com>
2025-01-15 18:53:03 -0500

    added root controller

---
+++ src/main/java/dev/resto/fal/controller/RootController.java
@@ -0,0 +1,15 @@
+package dev.resto.fal.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class RootController {
+
+    @GetMapping
+    public String root() {
+        return "";
+    }
+}
